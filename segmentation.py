7e6382d6203be2790b1f39ba07db1be4c5c332a0
Allen Abraham <allenabraham106@gmail.com>
2025-12-29 07:06:04 -0700

    Detects everything but the 5. prints out what it detects

--- segmentation.py
+++ segmentation.py
@@ -2,7 +2,7 @@ import numpy as np
 from skimage.transform import resize
 from skimage import measure
 from skimage.measure import regionprops
-from skimage.morphology import closing, square
+from skimage.morphology import closing, opening, square
 
 # from skimage.morphology import remove_small_objects
 import matplotlib.patches as patches
@@ -13,8 +13,10 @@ import cca
 license_plate = np.invert(
     cca.plate_like_objects[1]
 )  # hardcoded value, can be switched around
-# license_plate = closing(license_plate, square(3))
-# license_plate = remove_small_objects(license_plate, max_size=10)
+
+# open to remove noise and then close to fill
+license_plate = opening(license_plate, square(1))
+license_plate = closing(license_plate, square(1))
 labeled_plate = measure.label(license_plate)
 
 # show our images
@@ -25,10 +27,10 @@ ax1.imshow(license_plate, cmap="gray")
 h, w = license_plate.shape
 character_dimensions = (
     # Height is 35 - 60% of the plate
-    0.30 * h,
+    0.15 * h,
     0.80 * h,
     # Width is 1 - 15% of the plate
-    0.01 * w,
+    0.005 * w,
     0.15 * w,
 )
 
@@ -41,12 +43,15 @@ for region in regionprops(labeled_plate):
     y0, x0, y1, x1 = region.bbox
     region_height = y1 - y0
     region_width = x1 - x0
+    region_area = region.area
+    min_area = 50
 
     if (
         region_height > min_height
         and region_width > min_width
         and region_height < max_height
         and region_width < max_width
+        and region_area > min_area
     ):
         roi = license_plate[y0:y1, x0:x1]
 
@@ -59,6 +64,10 @@ for region in regionprops(labeled_plate):
         # resize character for character recognition
         resized_characters = resize(roi, (20, 20))
         character.append(resized_characters)
+
+        print(
+            f"Character at x={x0}: h={region_height:.1f}, w={region_width:.1f}, area={region_area}"
+        )
 
         # keep track on the arrangement of characters
         coloumn_list.append(x0)
